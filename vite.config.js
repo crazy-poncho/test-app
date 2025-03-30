8e3fa1c7bfcfd77b6e8d98498e0707ec2b8517d2
crazy-poncho <valentinivancikov@gmail.com>
2025-03-30 23:44:22 +0300

    chore: Setup vitest

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/tests/setup.js',
+  },
 });
