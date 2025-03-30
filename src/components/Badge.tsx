83652f39a6db30275e9863a362483ab3e67573cf
crazy-poncho <valentinivancikov@gmail.com>
2025-03-30 23:46:53 +0300

    feat: Add test locator

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,12 +1,19 @@
 import { FC, ReactNode } from 'react';
 
+import { useTestId } from '../hooks/useTestId';
+
 type BadgeProps = {
   value: ReactNode;
+  'data-testid'?: string;
 };
 
 export const Badge: FC<BadgeProps> = props => {
   return (
-    <div className='w-fit uppercase bg-sky-600 text-sky-100 px-2 py-1 text-[14px] rounded-xl'>
+    <div
+      data-component='badge'
+      data-testid={props['data-testid']}
+      className='w-fit uppercase bg-sky-600 text-sky-100 px-2 py-1 text-[14px] rounded-xl'
+    >
       <span>{props.value}</span>
     </div>
   );
